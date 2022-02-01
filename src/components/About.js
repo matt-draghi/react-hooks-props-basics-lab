2de75183ebe1ca816bfdb57680eaf5f41ade0ba4
Matt Draghi <mtd5264@gmail.com>
2022-02-01 17:37:02 -0500

    commit tree 14e41c811ecdbebbb2d0edb2ae5fbb008d31a069

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,14 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
-  const linkedin = props.links.linkedin
-  const github = props.links.github
+
   return (
     <div id="about">
       <h2>About Me</h2>
       {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
-      <Links github={github} linkedin={linkedin}/>
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
